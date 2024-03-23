c7c404a8d86c357ed7378b1829ff43b819495c04
CodCook <60304062@udst.edu.qa>
2024-03-23 11:24:51 +0300

    update the game

--- Star_game.py
+++ Star_game.py
@@ -2,7 +2,6 @@ import pygame as py
 import random
 import time
 
-
 py.init()
 
 WIDTH, HEIGHT = 800, 700
@@ -11,33 +10,47 @@ py.display.set_caption("Stars Game")
 
 background = py.transform.scale(py.image.load("pictures/game.jpg"), (WIDTH, HEIGHT))
 player_pic = py.transform.scale(py.image.load("pictures/player.png"), (50, 50))
-star_enemy = py.transform.scale(py.image.load("pictures/enemy.png"), (50, 50))
+star_enemy = py.transform.scale(py.image.load("pictures/dimond.webp"), (50, 50))
+dimonds_pic = py.transform.scale(py.image.load("pictures/enemy.png"), (50, 50))
+start_screen_img = py.transform.scale(py.image.load("pictures/startBG.jpg"), (WIDTH, HEIGHT))
 
+dimonds = []
 stars = []
 score = 0
 
 player = py.Rect(WIDTH/2, HEIGHT-50, 50, 50)
 player_val = 5
 star_speed = 3
+dimond_speed = 2
 
 clock = py.time.Clock()
 
-def draw(player, elapsed_time, stars):
+def draw(player, elapsed_time, stars, dimonds):
     WIN.blit(background, (0,0))
     WIN.blit(player_pic, player)
     time_text = py.font.SysFont("comicsans", 30).render(f"Time: {round(elapsed_time)}s", 1, (255, 255, 255))
+    score_text = py.font.SysFont("comicsans", 30).render(f"Score: {score}", 1, (255, 255, 255))
+    WIN.blit(score_text, (WIDTH - score_text.get_width() - 10, 10))
     WIN.blit(time_text, (10,10))
     for star in stars:
         WIN.blit(star_enemy, star)
+    for dimond in dimonds:
+        WIN.blit(dimonds_pic, dimond)
 
     py.display.flip()
     py.display.update()
+
+def spawn_dimond(num):
+    for _ in range(num):
+        x = random.randint(0, WIDTH-50)
+        y = random.randint(-50, -10)
+        dimonds.append(py.Rect(x, y, 50, 50))
 
 def spawn_star(num):
     for _ in range(num):
         x = random.randint(0, WIDTH-50)
         y = random.randint(-50, -10)
-        stars.append(py.Rect(x, y, 50, 50))
+        stars.append(py.Rect(x, y, 20, 20))
 
 def game_over(elapsed_time):
     run = True
@@ -54,11 +67,29 @@ def game_over(elapsed_time):
         WIN.fill((0, 0, 0))
         end_text = py.font.SysFont("comicsans", 30).render(f"Game Over! Time Survived: {round(elapsed_time)}s", 1, (255, 0, 0))
         WIN.blit(end_text, (WIDTH//2 - end_text.get_width()//2, HEIGHT//2 - end_text.get_height()//2))
+        scor_text = py.font.SysFont("comicsans", 30).render(f"Score: {score}", 1, (255, 255, 255))
+        WIN.blit(scor_text, (WIDTH//2 - scor_text.get_width()//2, HEIGHT//2 - scor_text.get_height()//2 + 50))
         again_text = py.font.SysFont("comicsans", 30).render("Play again? (Y/N)", 1, (255, 255, 255))
-        WIN.blit(again_text, (WIDTH//2 - again_text.get_width()//2, HEIGHT//2 + 50))
+        WIN.blit(again_text, (WIDTH//2 - again_text.get_width()//2, HEIGHT//2 + 70))
         py.display.update()
 
+def start_screen():
+    WIN.blit(start_screen_img, (0, 0))
+    start_text = py.font.SysFont("comicsans", 50).render("Press S to start the Game", 1, (255, 0, 0))
+    WIN.blit(start_text, (WIDTH//2 - start_text.get_width()//2, HEIGHT//2 - start_text.get_height()//2))
+    py.display.update()
+    waiting = True
+    while waiting:
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                py.quit()
+            elif event.type == py.KEYDOWN:
+                if event.key == py.K_s:
+                    waiting = False
+
 def main():
+    start_screen()
+    global score
     global stars
     while True:
         stars = []
@@ -74,23 +105,36 @@ def main():
             keys = py.key.get_pressed()
             if keys[py.K_LEFT] and player.x - player_val > 0:
                 player.x -= player_val
-            if keys[py.K_RIGHT] and player.x + player_val + player.width < WIDTH:
+            elif keys[py.K_RIGHT] and player.x + player_val + player.width < WIDTH:
                 player.x += player_val
+            elif keys[py.K_UP] and player.y - player_val > 0:
+                player.y -= player_val
+            elif keys[py.K_DOWN] and player.y + player_val + player.height < HEIGHT:
+                player.y += player_val
 
-            for star in stars:
+            for star in stars[:]:
                 star.y += star_speed
                 if star.colliderect(player):
                     run = False
+            for dim in dimonds[:]:
+                dim.y += dimond_speed
+                if dim.colliderect(player):
+                    score += 1
+                    dimonds.remove(dim)
 
-            draw(player, elapsed_time, stars)
+            draw(player, elapsed_time, stars, dimonds)
             if random.randint(1, 100) <= 2:
                 if elapsed_time > 30:
                     spawn_star(2)
+                    spawn_dimond(1)
                 elif elapsed_time > 60:
                     spawn_star(3)
+                    spawn_dimond(2)
                 elif elapsed_time > 120:
+                    spawn_dimond(3)
                     spawn_star(4)
                 else:
+                    spawn_dimond(1)
                     spawn_star(1)
 
         play_again = game_over(elapsed_time)
